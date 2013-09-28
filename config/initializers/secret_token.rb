506f2d5c072f866b8eb7222a4987264be27ec447
Florian Störkle <florian@stoerkle.net>
2013-09-28 21:38:37 +0200

    Create new Rails (v4) app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeverNotes::Application.config.secret_key_base = 'f05bdec77f67a0f12e4d7b3ed10ac65dfd6f19a8b08d7101dc9e036016234515e75b168d2985b5043b0be55f7fd616456a3f31992c467a4c28765a4612763f0a'
